c7518da71e8f412678f1102fe723510427c80244
Xiao Yu <jasonyux17@gmail.com>
2022-06-13 18:19:06 -0400

    updated login page

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 let login_button = document.getElementById("login-button")
 let login_form = document.getElementById("login-form")
+let login_error_div = document.getElementById("login-error-div")
 let display_login_form = false
 
 function check_if_logged_in() {
@@ -7,8 +8,7 @@ function check_if_logged_in() {
 	// when uploading, server will check cookie
 	let url = window.location.href
 	url = new URL(url);
-	logged_in = url.searchParams.get("loggedin") === 'true'
-	return logged_in
+	return url.searchParams.get("loggedin")
 }
 
 function toggle_login_form() {
@@ -22,11 +22,17 @@ function toggle_login_form() {
 	}
 }
 
+function toggle_login_error_div(){
+	login_error_div.classList.toggle('is-visible')
+}
+
 function configure_home_page() {
 	// configure logged in page
-	if (check_if_logged_in()) {
+	if (check_if_logged_in() === 'true') {
 		login_button.text = "Logged In"
 		login_button.style.pointerEvents = "none"
+	}else if(check_if_logged_in() === 'error'){
+		login_error_div.classList.add('is-visible')
 	}else{
 		login_button.text = "Login"
 	}
