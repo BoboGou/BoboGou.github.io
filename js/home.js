fb55fd1397c3c6402417feaa459b1827cb56f5b2
Xiao Yu <jasonyux17@gmail.com>
2022-06-13 16:24:42 -0400

    added login form

---
+++ js/home.js
@@ -0,0 +1,35 @@
+let login_button = document.getElementById("login-button")
+let login_form = document.getElementById("login-form")
+let display_login_form = false
+
+function check_if_logged_in() {
+	// just checking for the sake of displaying relevant content
+	// when uploading, server will check cookie
+	let url = window.location.href
+	url = new URL(url);
+	logged_in = url.searchParams.get("loggedin") === 'true'
+	return logged_in
+}
+
+function toggle_login_form() {
+	// toggle login form
+	if (display_login_form) {
+		login_form.style.display = "none"
+		display_login_form = false
+	}else{
+		login_form.style.display = "block"
+		display_login_form = true
+	}
+}
+
+function configure_home_page() {
+	// configure logged in page
+	if (check_if_logged_in()) {
+		login_button.text = "Logged In"
+		login_button.style.pointerEvents = "none"
+	}else{
+		login_button.text = "Login"
+	}
+}
+
+configure_home_page()
